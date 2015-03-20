97965726a2bb4492da7853a86fc9aead247eee1e
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-03-20 15:38:05 -0400

    moved over markdownHandler and rewrote it a bit associated with that

--- handlers.go
+++ handlers.go
@@ -2,3 +2,70 @@ package gnosis
 
 // package file will contain MarkdownHandler and RawHandler to handle incoming requests
 // Whole thing needs to be written
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"regexp"
+)
+
+type WikiPage struct {
+	Title string
+	//Body  string
+	Body template.HTML
+}
+
+var templates = template.Must(template.ParseFiles("wiki.html"))
+
+var wikiFilter = regexp.MustCompile("^/([a-zA-Z0-9_ /]+/)?([a-zA-Z0-9_ ]+)$")
+var fileFIlter = regexp.MustCompile("^/([a-zA-Z0-9_ /]+/)?([a-zA-Z0-9_ ]+)?\\.([a-zA-Z0-9_ ]+))?")
+
+func markdownHandler(responsePipe http.ResponseWriter, request *http.Request) {
+	filteredRequest := wikiFilter.FindStringSubmatch(request.URL.Path)
+	config := GetConfig()
+	if validRequest == nil {
+		log.Printf("null request improperly routed to wiki handler %s", request.URL.Path, config.mainserver.prefix)
+		http.Error(responsePipe, "Request not allowed", 403)
+	}
+
+	if filteredRequest[1] != config.mainserver.prefix {
+		log.Printf("request %s was improperly routed to wiki handler %s", request.URL.Path, config.mainserver.prefix)
+		http.Error(responsePipe, err.Error(), 500)
+	}
+
+	contents, err := ioutil.ReadFile(config.mainserver.prefix + filteredRequest[1] + ".md")
+	if err != nil {
+		log.Printf("request %s points to an bad file target sent to server %s", request.URL.Path, config.mainserver.prefix)
+		http.Error(responsePipe, err.Error(), 403)
+	}
+	// parse any markdown in the input
+	body := template.HTML(blackfriday.MarkdownCommon(contents))
+
+	response := WikiPage{Title: filteredRequest[2], Body: body}
+	err = templates.ExecuteTemplate(responsePipe, "wiki.html", response)
+	if err != nil {
+		http.Error(responsePipe, err.Error(), 500)
+	}
+}
+
+/*
+func contentHandler(responsePipe http.ResponseWriter, request *http.Request) {
+	validRequest := validFiles.FindStringSubmatch(request.URL.Path)
+	if validRequest == nil {
+		http.Error(responsePipe, "Request not allowed", 403)
+	}
+	strippedRequest := http.StripPrefix("/raw/", validRequest[0])
+	http.ServeFile(responsePipe, request, strippedRequest)
+	http.FileServer()
+	contents, err := loadFile(validRequest)
+	if err != nil {
+		http.Error(responsePipe, err, 500)
+	}
+	response := contents
+	err := templates.ExecuteTemplate(responsePipe, "markdown.html", p)
+	if err != nil {
+		http.Error(responsePipe, err.Error(), http.StatusInternalServerError)
+	}
+
+}
