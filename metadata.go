93091b4b163f7c28ce3e7c718c40cf06c25c90a4
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-04-11 16:29:18 -0400

    moved LoadPage up to the top of the file

--- metadata.go
+++ metadata.go
@@ -16,6 +16,71 @@ type PageMetadata struct {
 	Page     []byte
 }
 
+func (pdata *PageMetadata) LoadPage(pageName string) error {
+	f, err := os.Open(pageName)
+	reader := bufio.NewReader(f)
+	upperLine, fullLine, err := reader.ReadLine()
+
+	// inspect the first line you read
+	if err != nil {
+		return err
+	} else if !fullLine {
+		return err.New("first line I read wasn't a full line")
+	} else if lineIsTitle(upperLine) {
+		return err.New("first line looks an awful lot like the underside of the title o.O")
+	}
+
+	lowerLine, fullLine, err := reader.ReadLine()
+
+	// inspect the lower line
+	if err != nil {
+		return err
+	} else if !fullLine {
+		return err.New("second line I read wasn't a full line")
+	} else if lineIsTitle(lowerLine) {
+		// if the second line is a title, read the rest of the page in
+		// you don't have any metadata to work with here, move on
+		upperLine.Append('\n')
+		upperLine.Append(lowerLine)
+		upperLine.Append('\n')
+
+		_, err = reader.Read(lowerLine)
+		if err != nil {
+			return err
+		}
+
+		// you've successfully loaded the page - so return nothing
+		pdata.Loaded = true
+		return nil
+	}
+
+	// if you're at this point, the first line is metadata
+	// you gotta process it and work with the next line
+	// so let's just read through the file until we hit the title
+	for !lineIsTitle(lowerLine) {
+		// process the line
+		ProcessMetadata(upperLine)
+		// shift the lower line up
+		upperLine = lowerLine
+		// read in a new lower line
+		lowerLine, fullLine, err := reader.ReadLine()
+		if err != nil {
+			return err
+		} else if !fullLine {
+			return err.New("I filled my buffer with a line")
+		}
+	}
+
+	// by this point, I should have read everything in - let's read the rest and just return it
+	upperLine.Append('\n')
+	upperLine.Append(lowerLine)
+	upperLine.Append('\n')
+
+	_, err = reader.Read(lowerLine)
+	return err
+}
+
+// takes a single line of input and determines if it's a top level markdown header
 func (pdata *PageMetadata) lineIsTitle(line []byte) bool {
 	finalLength := len(line)
 	i := 0
@@ -147,68 +212,4 @@ func (pdata *PageMetadata) ProcessMetadata(line []byte) error {
 	pdata.checkMatch(line, "keyword", pdata.Keywords)
 	pdata.checkMatch(line, "keywords", pdata.Keywords)
 	pdata.checkMatch(line, "meta", pdata.Keywords)
-}
-
-func (pdata *PageMetadata) LoadPage(pageName string) error {
-	f, err := os.Open(pageName)
-	reader := bufio.NewReader(f)
-	upperLine, fullLine, err := reader.ReadLine()
-
-	// inspect the first line you read
-	if err != nil {
-		return err
-	} else if !fullLine {
-		return err.New("first line I read wasn't a full line")
-	} else if lineIsTitle(upperLine) {
-		return err.New("first line looks an awful lot like the underside of the title o.O")
-	}
-
-	lowerLine, fullLine, err := reader.ReadLine()
-
-	// inspect the lower line
-	if err != nil {
-		return err
-	} else if !fullLine {
-		return err.New("second line I read wasn't a full line")
-	} else if lineIsTitle(lowerLine) {
-		// if the second line is a title, read the rest of the page in
-		// you don't have any metadata to work with here, move on
-		upperLine.Append('\n')
-		upperLine.Append(lowerLine)
-		upperLine.Append('\n')
-
-		_, err = reader.Read(lowerLine)
-		if err != nil {
-			return err
-		}
-
-		// you've successfully loaded the page - so return nothing
-		pdata.Loaded = true
-		return nil
-	}
-
-	// if you're at this point, the first line is metadata
-	// you gotta process it and work with the next line
-	// so let's just read through the file until we hit the title
-	for !lineIsTitle(lowerLine) {
-		// process the line
-		ProcessMetadata(upperLine)
-		// shift the lower line up
-		upperLine = lowerLine
-		// read in a new lower line
-		lowerLine, fullLine, err := reader.ReadLine()
-		if err != nil {
-			return err
-		} else if !fullLine {
-			return err.New("I filled my buffer with a line")
-		}
-	}
-
-	// by this point, I should have read everything in - let's read the rest and just return it
-	upperLine.Append('\n')
-	upperLine.Append(lowerLine)
-	upperLine.Append('\n')
-
-	_, err = reader.Read(lowerLine)
-	return err
 }
