945f02df79d78efe7ba389aca91f7dd713779d7a
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-04-12 03:22:40 -0400

    got all checks within TestCheckMatch working

--- metadata_test.go
+++ metadata_test.go
@@ -50,11 +50,54 @@ func TestLineIsTitle(t *testing.T) {
 	assert.False(t, pdata.lineIsTitle(titleLine), "the underlining has to be continous - no tabs - so this should have failed")
 }
 
+func TestCheckMatch(t *testing.T) {
+	pdata := new(PageMetadata)
+
+	metadataLine := []byte("topic = a")
+	metadataMatch := []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["a"], "should have been able to add element a to the thingy")
+
+	metadataLine = []byte("topic= b")
+	metadataMatch = []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["b"], "should have been able to add element b to the thingy")
+
+	metadataLine = []byte("topic=c")
+	metadataMatch = []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["c"], "should have been able to add element c to the thingy")
+
+	metadataLine = []byte("topic=d e f")
+	metadataMatch = []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["d-e-f"], "should have been able to add element d e f to the thingy")
+
+	metadataLine = []byte("topic=g  h")
+	metadataMatch = []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["g-h"], "should have been able to add element g-h to the thingy")
+
+	metadataLine = []byte("topic:i")
+	metadataMatch = []byte("topic")
+	pdata.checkMatch(metadataLine, metadataMatch, &pdata.Topics)
+	assert.True(t, pdata.Topics["i"], "should have been able to add element i to the thingy")
+}
+
+/*
 func TestProcessMetadata(t *testing.T) {
 	pdata := new(PageMetadata)
 
-	metaDataLine := []byte("topic=a")
+	metaDataLine := []byte("topic = a")
 	pdata.processMetadata(metaDataLine)
 	assert.True(t, stringKeyExistsInMap(pdata.Topics, "a"), "the element I just tried to add should have been added")
-	assert.True(t, pdata.Topics["a"], , "the element I just tried to add should have been added")
-}
+	assert.True(t, pdata.Topics["a"], "the element I just tried to add should be true")
+	assert.False(t, pdata.Topics["b"], "this is a trap")
+
+	/*
+		metaDataLine = []byte("topic=a")
+		pdata.processMetadata(metaDataLine)
+		assert.True(t, stringKeyExistsInMap(pdata.Topics, "a"), "the element I just tried to add should have been added")
+		assert.True(t, pdata.Topics["a"], "the element I just tried to add should have been added")
+*/
+//}
