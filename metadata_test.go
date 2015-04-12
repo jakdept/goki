8fc85d3d57b24012cb6aa5342119e5ce3506d9fc
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-04-11 21:54:38 -0400

    added test for TestLineIsTitle

--- metadata_test.go
+++ metadata_test.go
@@ -1,3 +1,42 @@
 // contains the tests for the metadata tests
 
 package gnosis
+
+import (
+	"github.com/stretchr/testify/assert"
+	//"os"
+	//"path"
+	"testing"
+)
+
+func TestLineIsTitle(t *testing.T) {
+	pd := new(PageMetadata)
+
+	// test the most normal topic line I'd expect
+	titleLine := []byte("=======")
+	assert.True(t, pd.lineIsTitle(titleLine), "the most basic topic line just failed")
+
+	titleLine = []byte("=")
+	assert.True(t, pd.lineIsTitle(titleLine), "one = should be enough")
+
+	titleLine = []byte("   ======")
+	assert.True(t, pd.lineIsTitle(titleLine), "any spaces before the heading portion should not cause failure")
+
+	titleLine = []byte("			======")
+	assert.True(t, pd.lineIsTitle(titleLine), "tabs before the heading portion should not cause failure")
+
+	titleLine = []byte("=======     ")
+	assert.True(t, pd.lineIsTitle(titleLine), "spaces after the heading portion should not cause failure")
+
+	titleLine = []byte("=======			")
+	assert.True(t, pd.lineIsTitle(titleLine), "tabs after the heading portion should not cause failure")
+
+	titleLine = []byte("=======\n")
+	assert.True(t, pd.lineIsTitle(titleLine), "a newline after the heading portion should not cause failure")
+
+	titleLine = []byte("===== ===")
+	assert.False(t, pd.lineIsTitle(titleLine), "the underlining has to be continous - no spaces - so this should have failed")
+
+	titleLine = []byte("====	=====")
+	assert.False(t, pd.lineIsTitle(titleLine), "the underlining has to be continous - no tabs - so this should have failed")
+}
