6e810295f4e46d7f9a5c7e68a61af5bc859ff1b3
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-08-19 22:58:42 -0400

    finished reworking `TestGenerateWikiFromFile`
    * i'm not sure if i like the upstream behavior
    * that function fully works

--- index_test.go
+++ index_test.go
@@ -3,12 +3,13 @@ package gnosis
 
 import (
 	"github.com/stretchr/testify/assert"
-	"os"
-	// "bytes"
-	"path"
+	// "os"
+	"bytes"
+	"encoding/json"
+	// "path"
 	"path/filepath"
 	"testing"
-	"log"
+	// "log"
 	// "time"
 	"io/ioutil"
 	"strings"
@@ -53,33 +54,88 @@ func TestCleanupMarkdownFiles(t *testing.T) {
 
 		rawData, err := ioutil.ReadFile(input)
 		if err != nil {
-			t.Errorf("Failed to open input file [%q] - %s", input, err)
+			t.Errorf("failed to open input file [%q] - %s", input, err)
 			return
 		}
 
 		expectedData, err := ioutil.ReadFile(output)
 		if err != nil {
-			t.Errorf("Failed to open input file [%q] - %s", output, err)
+			t.Errorf("failed to open input file [%q] - %s", output, err)
 			return
 		}
 
 		cleanData := cleanupMarkdown(rawData)
 		if cleanData != string(expectedData) {
-			splitPath:= strings.Split(output, string(os.PathSeparator))
-			filepath := path.Join(os.TempDir(), splitPath[len(splitPath)-1])
-			f, err := os.Create(filepath)
+			f, err := ioutil.TempFile("", "cleanupMarkdown.")
 			if err != nil {
 				t.Fatal(err)
 			}
-			if _, err := f.WriteString(cleanData); err != nil {
-				t.Fatal(err)
-			}
-			if err := f.Close(); err != nil {
+			if _, err := f.Write(rawData); err != nil {
 				t.Fatal(err)
 			}
 			t.Errorf("Input [%q] gave incorrect output against [%q] - wrote to [%q]",
-				input, output, filepath)
+				input, output, f.Name())
+			if err := f.Close(); err != nil {
+				t.Fatal(err)
+			}
 		}
 	}
+}
 
+func TestGenerateWikiFromFile(t *testing.T) {
+	var input string
+	defer func() {
+		if err := recover(); err != nil {
+			t.Errorf("\npanic while processing [%#v]\n", input)
+		}
+	}()
+
+	inputFiles, err := filepath.Glob("./testfiles/*.md")
+	if err != nil {
+		t.Errorf("Failed to find all markdown files - %s", err)
+		return
+	}
+
+	for _, input = range inputFiles {
+		output := strings.TrimSuffix(input, ".md") + ".generateWikiFromFile"
+
+		expectedRaw, err := ioutil.ReadFile(output)
+		if err != nil {
+			t.Errorf("Failed to open input file [%q] - %s", output, err)
+			return
+		}
+
+		var expectedValue indexedPage
+		err = json.Unmarshal(expectedRaw, &expectedValue)
+		if err != nil {
+			t.Errorf("Failed to unmarshal the expected values - %s", err)
+		}
+
+		expectedRaw, err = json.Marshal(expectedValue) 
+		if err != nil {
+			t.Errorf("Failed to marshal the expected values - %s", err)
+		}
+
+		actualValue, err := generateWikiFromFile(input, "teststring", []string{})
+		actualRaw, err := json.Marshal(actualValue)
+		if err != nil {
+			t.Errorf("Failed to marshal the actual values - %s", err)
+		}
+
+		if bytes.Compare(actualRaw, expectedRaw) != 0 {
+			f, err := ioutil.TempFile("", "generateWikiFromFile.")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if _, err := f.Write(actualRaw); err != nil {
+				t.Fatal(err)
+			}
+			t.Errorf("Input [%q] gave incorrect output against [%q]\nwrote to [%q]",
+				input, output, f.Name())
+			t.Errorf("\ninput is  [%s]\noutput is [%s]", actualRaw, expectedRaw)
+			if err := f.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
 }
---
+++ testfiles/test1.md
@@ -0,0 +1,22 @@
+This is a page
+==============
+
+* It has some lists
+* Lots of lists
+* Even more
+
+Numbered List
+-------------
+
+warning> Be careful, there is a numbered list ahead
+
+1. It has a numbered list
+2. There are a few items
+4. On the numbered list
+3. Of note, this list
+5. Is out of order
+
+## This section has quotes
+
+> This is a very important quote. It is insightful
+> and helpful. - anonymous
